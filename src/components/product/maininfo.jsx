e2b89b5593a02a7f80b5dd3dd76a915416fb5358
smart306 <smart2014uk@gmail.com>
2026-03-15 13:33:44 +0100

    adaptivity

--- src/components/product/maininfo.jsx
+++ src/components/product/maininfo.jsx
@@ -7,8 +7,8 @@ import { Carousel, CarouselContent, CarouselItem } from "../ui/carousel";
 export default function MainInfo({data}){
     return (
       <section className="bg-primary flex flex-col lg:flex-row w-full h-fit pt-12 lg:pt-28">
-        <div className="hidden sm:flex flex-row lg:flex-col space-x-2 w-full p-4">
-          <div className="relative bg-white aspect-square w-full rounded-2xl">
+        <div className="hidden sm:flex flex-row lg:flex-col w-full space-x-2 lg:space-x-0 lg:w-fit p-4">
+          <div className="relative bg-white aspect-square w-full lg:w-lg xl:w-xl rounded-2xl">
             <Image
               src={data.image1}
               alt="p"
@@ -16,8 +16,8 @@ export default function MainInfo({data}){
               className="object-contain p-4"
             />
           </div>
-          <div className="flex flex-col lg:flex-row w-1/3 lg:w-full space-y-2 lg:space-x-2 lg:p-2">
-            <div className="relative bg-white aspect-square w-full rounded-2xl">
+          <div className="flex flex-col lg:flex-row w-1/3 lg:w-lg xl:w-xl space-y-2 lg:space-x-2 lg:py-2">
+            <div className="relative bg-white aspect-square w-full lg:h-11/12 rounded-2xl">
               <Image
                 src={data.image1}
                 alt="p"
@@ -25,7 +25,7 @@ export default function MainInfo({data}){
                 className="object-contain p-4"
               />
             </div>
-            <div className="relative bg-white aspect-square w-full rounded-2xl">
+            <div className="relative bg-white aspect-square w-full lg:h-11/12 rounded-2xl">
               <Image
                 src={data.image1}
                 alt="p"
@@ -33,7 +33,7 @@ export default function MainInfo({data}){
                 className="object-contain p-4"
               />
             </div>
-            <div className="relative bg-white aspect-square w-full rounded-2xl">
+            <div className="relative bg-white aspect-square w-full lg:h-11/12 rounded-2xl">
               <Image
                 src={data.image1}
                 alt="p"
@@ -89,8 +89,8 @@ export default function MainInfo({data}){
             </CarouselContent>
           </Carousel>
         </div>
-        <div className="w-full h-full py-2 lg:py-8 px-4 flex flex-col justify-between items-center text-center">
-          <div className="w-full flex flex-col justify-center items-center space-y-4">
+        <div className="w-full py-2 lg:py-4 px-4 flex flex-col justify-between items-center text-center">
+          <div className="w-full py-8 flex flex-col items-center space-y-4 xl:space-y-2">
             <h1 className="h1 font-secondary">{data.title}</h1>
             <div className="flex">
               <Star className="text-border" />
@@ -99,9 +99,9 @@ export default function MainInfo({data}){
               <Star className="text-border" />
               <StarHalf className="text-border" />
             </div>
-            <p className="p font-secondary">{data.description}</p>
+            <p className="h3 font-secondary">{data.description}</p>
           </div>
-          <div className="w-full space-y-4">
+          <div className="w-full space-y-4 flex flex-col justify-end py-8">
             <div className="flex gap-4 lg:gap-2 justify-center">
               {data.colors.map((item) => (
                 <div key={item.id}>
@@ -115,7 +115,7 @@ export default function MainInfo({data}){
             </div>
             <div className="w-full space-y-4 sm:space-y-2">
               <p className="h2 font-secondary">{data.price}</p>
-              <div className="flex flex-col space-y-4 sm:sapce-y-0 sm:flex-row w-full">
+              <div className="flex flex-col space-y-4 sm:space-y-0 sm:flex-row w-full">
                 <Button className="px-4 py-2 rounded-full text-base lg:text-2xl w-full sm:w-1/2">
                   Додати до кошику
                 </Button>
